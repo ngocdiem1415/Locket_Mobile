a712a45b517d391092b82a0c8c78b1e1fe1add87
Võ Nguyễn Thanh Diệu <22130039@st.hcmuaf.edu.vn>
2025-05-04 13:58:46 +0700

    all image

--- Locket_Mobile/app/src/main/java/com/hucmuaf/locket_mobile/MainActivity.java
+++ Locket_Mobile/app/src/main/java/com/hucmuaf/locket_mobile/MainActivity.java
@@ -1,6 +1,8 @@
 package com.hucmuaf.locket_mobile;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,6 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
     }
 }
