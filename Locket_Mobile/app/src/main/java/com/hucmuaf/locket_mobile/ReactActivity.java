5097d641ed7cd6e1ac413d875c4b9903a48f4cf7
Phung Khanh Duy <22130062@st.hcmuaf.edu.vn>
2025-05-06 17:52:17 +0700

    feat: - Thêm giao diện chụp ảnh chính. - Thêm giao diện hiển thị ảnh và react emoji. - Tạo sự kiện lướt lên để chuyển từ giao diện chụp ảnh sang giao diện xem. - Thêm chức năng hiển thị camera và đổi chiều camera.

---
+++ Locket_Mobile/app/src/main/java/com/hucmuaf/locket_mobile/ReactActivity.java
@@ -0,0 +1,24 @@
+package com.hucmuaf.locket_mobile;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ReactActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.react_emoji_comment);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
