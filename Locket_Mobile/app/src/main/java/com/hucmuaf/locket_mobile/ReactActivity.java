7475ba25ca5fba9678c7ddedfadaf047c1221d47
Phung Khanh Duy <22130062@st.hcmuaf.edu.vn>
2025-05-09 19:04:29 +0700

    feat: Thêm hiển thị danh sách bạn bè khi nhấn vào thanh tất cả bạn bè. Tạo lớp phủ để phân biệt.

--- Locket_Mobile/app/src/main/java/com/hucmuaf/locket_mobile/ReactActivity.java
+++ Locket_Mobile/app/src/main/java/com/hucmuaf/locket_mobile/ReactActivity.java
@@ -8,8 +8,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.hucmuaf.locket_mobile.model.ItemFriend;
+import com.hucmuaf.locket_mobile.model.ItemFriendAdapter;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class ReactActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private ItemFriendAdapter itemAdapter;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +33,20 @@ public class ReactActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        recyclerView = findViewById(R.id.list_friends);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        List<ItemFriend> list = Arrays.asList(
+          new ItemFriend("account_icon", "Khanh Duy"),
+          new ItemFriend("account_icon", "Bích Loan"),
+          new ItemFriend("account_icon", "Tấn Lực"),
+          new ItemFriend("account_icon", "Thanh Diệu")
+        );
+
+        itemAdapter = new ItemFriendAdapter(this, list);
+
+        recyclerView.setAdapter(itemAdapter);
+
     }
 }
