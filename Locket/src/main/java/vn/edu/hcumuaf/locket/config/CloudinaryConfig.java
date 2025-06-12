560b3c2af23a070ddc51c6aed74f8b0d2416d440
NgocDiem <22130038@st.hcmuaf.edu.vn>
2025-06-12 22:46:00 +0700

    update gitinore and fix key in properties

--- Locket/src/main/java/vn/edu/hcumuaf/locket/config/CloudinaryConfig.java
+++ Locket/src/main/java/vn/edu/hcumuaf/locket/config/CloudinaryConfig.java
@@ -1,6 +1,7 @@
 package vn.edu.hcumuaf.locket.config;
 
 import com.cloudinary.Cloudinary;
+import com.google.api.client.util.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,12 +11,21 @@ import java.util.Map;
 @Configuration
 public class CloudinaryConfig {
 
+    @Value("${cloudinary.cloud_name}")
+    private String cloudName;
+
+    @Value("${cloudinary.api_key}")
+    private String apiKey;
+
+    @Value("${cloudinary.api_secret}")
+    private String apiSecret;
+
     @Bean
     public Cloudinary configKey(){
         Map<String,String> config = new HashMap();
-        config.put("cloud_name", "dwjztnzgv");
-        config.put("api_key", "191659792121745");
-        config.put("api_secret", "APplbFE3u0xbYmIegZu0SC7PIoY");
+        config.put("cloud_name", cloudName);
+        config.put("api_key", apiKey);
+        config.put("api_secret", apiSecret);
         return new Cloudinary(config);
     }
 }
