ac4df38dde0f6136f752793fe4f078d6dc622d51
Phung Khanh Duy <22130062@st.hcmuaf.edu.vn>
2025-06-13 21:45:36 +0700

    fix cấu hình firebase

--- Locket_Mobile/app/build.gradle.kts
+++ Locket_Mobile/app/build.gradle.kts
@@ -61,16 +61,7 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:33.13.0"))
     implementation("com.google.firebase:firebase-auth")
 
-<<<<<<< HEAD
-dependencies {
-    implementation ("com.github.bumptech.glide:glide:4.16.0")
-    annotationProcessor ("com.github.bumptech.glide:compiler:4.16.0")
-}
-dependencies {
-    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
-}
-=======
+
     // Glide for image loading
     implementation("com.github.bumptech.glide:glide:4.16.0")
     annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
@@ -81,5 +72,3 @@ dependencies {
     implementation("com.google.code.gson:gson:2.10.1")
 }
 
-
->>>>>>> origin/main
