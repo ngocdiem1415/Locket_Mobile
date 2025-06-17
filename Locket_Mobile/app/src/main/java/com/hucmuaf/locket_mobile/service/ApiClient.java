0bf0f7489d3d1a7b7eb891230da5f020df6593e7
NgocDiem <22130038@st.hcmuaf.edu.vn>
2025-06-17 11:38:39 +0700

    fix errors view

--- Locket_Mobile/app/src/main/java/com/hucmuaf/locket_mobile/service/ApiClient.java
+++ Locket_Mobile/app/src/main/java/com/hucmuaf/locket_mobile/service/ApiClient.java
@@ -9,8 +9,8 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
     //     private static final String BASE_URL = "http://10.50.0.1:8080/"; // For Android emulator
-//    private static final String BASE_URL = "http://10.0.100.23:8080/"; // For real device, your computer's IP
-     private static final String BASE_URL = "http://localhost:8080/"; // For testing
+    private static final String BASE_URL = "http://10.0.100.23:8080/"; // For real device, your computer's IP
+//     private static final String BASE_URL = "http://localhost:8080/"; // For testing
 
     private static Retrofit retrofit = null;
     private static FriendListApiService friendListApiService = null;
